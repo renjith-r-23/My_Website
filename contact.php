cd3bacf961d3c00370547d144c48b6bd23f1bfd7
Renjith R <145246224+renjith-r-23@users.noreply.github.com>
2023-11-14 11:52:51 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    $to = 'renjithnrd3@gmail.com'; // Change this to your email
+    $subject = 'New contact form submission';
+    $body = "Name: $name\nEmail: $email\n\n$message";
+
+    if (mail($to, $subject, $body)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Message could not be sent.";
+    }
+}
+?>
